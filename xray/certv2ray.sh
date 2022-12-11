5f1c7d25605b5bfdac617a70681d11acea73571d
owl64 <89740504+owl64@users.noreply.github.com>
2022-12-11 12:43:12 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -17,13 +17,15 @@ IZIN=$( curl https://raw.githubusercontent.com/AkbarStoreVPN/perizinan/main/ipvp
 clear
 echo start
 sleep 0.5
-source /var/lib/akbarstorevpn/ipvps.conf
-domain=$IP
-systemctl enable xray.service
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
-systemctl restart xray.service
-echo Done
-sleep 0.5
-clear 
-neofetch
+source /var/lib/crot/ipvps.conf
+domain=$(cat /etc/xray/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m kimochilol@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
