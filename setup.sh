59c65586d03a0710c307ea7cdeef09b00abe7d0c
owl64 <89740504+owl64@users.noreply.github.com>
2022-12-22 20:00:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -85,7 +85,7 @@ rm -f /root/ohp.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://t.me/Akbar218
+Documentation=https://t.me/owl64
 
 [Service]
 Type=oneshot
